32c24fbc4aedf517388abf0ef0b9d387f3dd1392
akiyamart <akiyamah@yandex.ru>
2023-12-20 23:04:14 +0300

    Remove mistakes

--- src/lab4/task1/main.py
+++ src/lab4/task1/main.py
@@ -1,21 +1,29 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+movies_file_path = os.path.join(BASE_DIR, 'movies.txt')
+views_file_path = os.path.join(BASE_DIR, 'views.txt')
 
 class Movie:
+    """Класс для представления фильма."""
     def __init__(self, movie_id, title):
+        """Инициализация фильма."""
         self.movie_id = movie_id
         self.title = title
 
     def __str__(self):
+        """Строковое представление фильма."""
         return f"{self.movie_id}, {self.title}"
 
-
 class MovieRecommendationSystem:
+    """Система рекомендации фильмов."""
     def __init__(self, movies_filename, views_filename):
+        """Инициализация системы."""
         self.movies = self.load_movies(movies_filename)
         self.views = self.load_views(views_filename)
 
     def load_movies(self, filename):
+        """Загрузка данных о фильмах."""
         movies = []
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
@@ -24,6 +32,7 @@ class MovieRecommendationSystem:
         return movies
 
     def load_views(self, filename):
+        """Загрузка данных о просмотрах."""
         views = []
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
@@ -31,12 +40,14 @@ class MovieRecommendationSystem:
         return views
 
     def recommend_movie(self, user_views):
+        """Рекомендация фильма на основе просмотров пользователя."""
         matching_users = self.find_matching_users(user_views)
         remaining_movies = self.find_remaining_movies(user_views, matching_users)
         recommended_movie = self.choose_recommended_movie(remaining_movies)
         return recommended_movie.title if recommended_movie else None
 
     def find_matching_users(self, user_views):
+        """Поиск пользователей с похожей историей просмотров."""
         matching_users = []
         for i, views in enumerate(self.views):
             common_views = set(user_views) & set(views)
@@ -45,6 +56,7 @@ class MovieRecommendationSystem:
         return matching_users
 
     def find_remaining_movies(self, user_views, matching_users):
+        """Поиск непросмотренных фильмов, просмотренных похожими пользователями."""
         viewed_movies = set(user_views)
         remaining_movies = []
         for i in matching_users:
@@ -52,6 +64,7 @@ class MovieRecommendationSystem:
         return list(set(remaining_movies))
 
     def choose_recommended_movie(self, remaining_movies):
+        """Выбор рекомендованного фильма на основе количества просмотров."""
         movie_counts = {}
         for movie_id in remaining_movies:
             users_watched = set()
@@ -66,19 +79,24 @@ class MovieRecommendationSystem:
 
         max_count = max(movie_counts.values())
         top_movies = [movie_id for movie_id, count in movie_counts.items() if count == max_count]
-        return self.movies[top_movies[0]-1] if top_movies else None
-
+        return self.movies[top_movies[0] - 1] if top_movies else None
 
 if __name__ == "__main__":
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    movies_file_path = os.path.join(BASE_DIR, 'movies.txt')
-    views_file_path = os.path.join(BASE_DIR, 'views.txt')
-
     recommendation_system = MovieRecommendationSystem(movies_file_path, views_file_path)
 
-    user_input = input("Введите историю просмотров через запятую: ")
-    user_history = list(map(int, user_input.split(',')))
+    while True:
+        try:
+            user_input = input("Введите историю просмотров через запятую: ")
+            user_history = list(map(int, user_input.split(',')))
+
+            if user_history:
+                break
+            else:
+                print("Пожалуйста, введите хотя бы один элемент.")
+        except ValueError:
+            print("Некорректный ввод")
 
     recommended_film = recommendation_system.recommend_movie(user_history)
 
     print(f"Рекомендуемый фильм: {recommended_film}")
+
--- src/lab4/task2/main.py
+++ src/lab4/task2/main.py
@@ -1,52 +1,69 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+input_file = os.path.join(BASE_DIR, 'input_resp.txt')
+import os
 
 class SurveyParticipant:
+    """Класс для представления участника опроса."""
     def __init__(self, name, age):
+        """Инициализация участника опроса."""
         self.name = name
         self.age = age
 
     def __lt__(self, other):
+        """Сравнение участников опроса."""
         return (self.age, self.name) > (other.age, other.name)
 
     def __str__(self):
+        """Строковое представление участника опроса."""
         return f"{self.name} ({self.age})"
 
+
 class AgeGroup:
+    """Класс для представления возрастной группы."""
     def __init__(self, name, min_age, max_age):
+        """Инициализация возрастной группы."""
         self.name = name
         self.min_age = min_age
         self.max_age = max_age
         self.participants = []
 
     def add_participant(self, participant):
+        """Добавление участника в возрастную группу."""
         self.participants.append(participant)
 
     def __str__(self):
+        """Строковое представление возрастной группы."""
         participants_info = ', '.join(str(participant) for participant in sorted(self.participants))
         return f"{self.name}: {participants_info}"
 
+
 class AgeGroupsManager:
+    """Класс для управления возрастными группами."""
     def __init__(self, boundaries):
+        """Инициализация управляющего класса."""
         self.groups = [
-            AgeGroup(f"{boundaries[i - 1] + 1}-{boundaries[i]}", boundaries[i - 1] + 1, boundaries[i]) 
+            AgeGroup(f"{boundaries[i - 1] + 1}-{boundaries[i]}", boundaries[i - 1] + 1, boundaries[i])
             for i in range(len(boundaries) - 1, 0, -1)
         ]
         self.groups.append(AgeGroup(f"0-{boundaries[0]}", 0, boundaries[0]))
         self.groups.append(AgeGroup(f"{boundaries[-1] + 1}+", boundaries[-1] + 1, float('inf')))
 
     def add_participant(self, participant):
+        """Добавление участника в соответствующую возрастную группу."""
         for group in self.groups:
             if group.min_age <= participant.age <= group.max_age:
                 group.add_participant(participant)
                 break
 
     def get_age_groups_info(self):
+        """Получение информации о возрастных группах."""
         sorted_groups = sorted(self.groups, key=lambda x: x.min_age, reverse=True)
         return [str(group) for group in sorted_groups if group.participants]
 
     def read_participants_from_file(self, file_path):
+        """Чтение участников из файла и добавление их в соответствующие возрастные группы."""
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 if line.strip() == "END":
@@ -56,8 +73,8 @@ class AgeGroupsManager:
                     participant = SurveyParticipant(name, int(age))
                     self.add_participant(participant)
 
+
 if __name__ == "__main__":
-    input_file = os.path.join(BASE_DIR, 'input_resp.txt')
     age_boundaries = list(map(int, input('Введите возрастные границы:').split()))
     age_groups_manager = AgeGroupsManager(age_boundaries)
     age_groups_manager.read_participants_from_file(input_file)
